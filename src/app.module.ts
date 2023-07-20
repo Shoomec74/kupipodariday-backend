08803caab8264cb9a21b0866da64b31fdcd87ddd
Evgeny Rusakov <shoomec@yandex.ru>
2023-07-21 03:09:26 +0500

    Before the first review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
+import { User } from './users/entity/user.entity';
+import { Wish } from './wishes/entity/wishes.entity';
+import { Wishlist } from './wishlists/entity/wishlist.entity';
+import { Offer } from './offers/entity/offers.entity';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { HashModule } from './hash/hash.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [User, Wish, Wishlist, Offer],
+      synchronize: true,
+    }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+    AuthModule,
+    HashModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
 })
 export class AppModule {}
